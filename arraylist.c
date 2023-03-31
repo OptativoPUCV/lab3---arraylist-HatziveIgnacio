d66e2e0e1fe2c58bfc4a54677946e6868639d9c9
Replit user <>
2023-03-31 17:35:02 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -53,8 +53,7 @@ void push(ArrayList * l, void * data, int i)
   //
   // mover a la derecha
   int aux_size = l->size;
-  for (aux_size; aux_size > i; aux_size--)
-    {
+  for (aux_size; aux_size > i; aux_size--){
       l->data[aux_size+1] = l->data[aux_size];
     }
   
